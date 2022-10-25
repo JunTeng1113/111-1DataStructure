fda0b4f2b7c026efa2567c9da94e19a4c300519e
JunTeng1113 <JunTeng.1113@gmail.com>
2022-10-26 03:32:48 +0800

    feat: 新增第十三題 迴文問題

---
+++ midterm/midterm/src/q13.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// q13. 迴文問題
+public class q13 {
+    public static void main(String[] args) throws Exception {
+        System.out.print("輸入一字元為: ");
+        BufferedReader bfReader = new BufferedReader(new InputStreamReader(System.in));
+        String str1 = bfReader.readLine();
+        String str2 = str1;
+        StringBuilder stringbuilder = new StringBuilder();
+        stringbuilder.append(str1);
+        stringbuilder.reverse();
+
+        if (str2.equals(stringbuilder.toString())) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
