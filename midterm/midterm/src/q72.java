9aaac9b1677a08b410e0c6335be63e883d0503d1
JunTeng1113 <JunTeng.1113@gmail.com>
2022-10-26 10:04:39 +0800

    feat: 新增第七十二題 紙菸

---
+++ midterm/midterm/src/q72.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// q72. 紙菸
+public class q72 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bfReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("請輸入n: ");
+        int n = Integer.parseInt(bfReader.readLine());
+        System.out.print("請輸入k: ");
+        int k = Integer.parseInt(bfReader.readLine());
+
+        System.out.print(calc(n, k, 0));
+    }
+    public static int calc(int a, int b, int i) {
+        if (a + (i / b) > 0) return a + (i / b) + calc((a / b), b, (i % b) + (a % b));
+        return 0;
+    }
+}
